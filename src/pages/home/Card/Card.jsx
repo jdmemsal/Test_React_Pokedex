1de655c81b15a9393b20023171e85c8e115c3fe6
jdmemsal <jdmembrive@gmail.com>
2024-07-08 20:48:40 +0200

    it adds the pk types and characteristics, plus general card styles

--- src/pages/home/Card/Card.jsx
+++ src/pages/home/Card/Card.jsx
@@ -28,6 +28,7 @@ const Card = ({card}) => {
             try {
                 const idUrl = card.url.split("/");
                 const apiPokemon = await axios.get(`${URL_SPECIES}/${idUrl[6]}`);
+                //const apiPokemon = await axios.get(`${URL_SPECIES}`);
 
                 setPkSpecie(apiPokemon.data);
 
@@ -39,31 +40,38 @@ const Card = ({card}) => {
         spPokemon();
     }, []);
 
-
+//console.log(pokeObject)
+//console.log(pkSpecie)
     return (
-        <div className={css.cards}>
+        <div className={css.cards_container}>
             {/* .sprites.other["official-artwork"].front_default
             This is how the data comes from the api, to see it, do a console.log(pokeObject)
             and look for the path, meaning, go to sprites, other, etc. We are just accessing the data in the object */}
             <img className={css.img_pk} src={pokeObject?.sprites?.other["official-artwork"]?.front_default} alt='pokemonImage'/>
             
             <div className={css.sub_cards}>
-                <h4><strong className={css.id_pk}>number</strong></h4>
-                <h3><strong className={css.name_pk}>name</strong></h3>
-                <p className={css.size_pk}>size</p>
-                <p className={css.weight_pk}>weight</p>
-                <p className={css.habitat_pk}>habitat</p>
+                <h4><strong className={css.id_pk}>Nº {pokeObject.id}</strong></h4>
+                <h3><strong className={css.name_pk}>{pokeObject.name}</strong></h3>
+                <p className={css.size_pk}>{`${(pokeObject.height * 0.1).toFixed(1)} M`}</p>
+                <p className={css.weight_pk}>{`${(pokeObject.weight * 0.1).toFixed(1)} Kg`}</p>
+                <p className={css.habitat_pk}>Habitat: {pkSpecie?.habitat?.name}</p>
 
                 {/*stats*/}
-                <div>
+                <div className={css.div_stats_container}>
                     {pokeObject?.stats?.map((att, index) => {
                         return(
-                            <h5 key={index}>
-                                <span>{att.stat.name}</span>
+                            <h5 key={index} className={css.att_stats}>
+                                <span className={css.name_stat}>{att.stat.name}</span>
                                 <progress value={att.base_stat} max={120}></progress>
-                                <span>{att.base_stat}</span>
+                                <span className={css.value_stat}>{att.base_stat}</span>
                             </h5>
                         );
+                    })}
+                </div>
+
+                <div className={css.div_type_container}>
+                    {pokeObject?.types?.map((ty, index) => {
+                        return <h6 key={index} className={css.div_type_name}>{ty.type.name}</h6>
                     })}
                 </div>
             </div>
